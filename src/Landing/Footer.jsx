48cb0df05d30939886d46d521a485468282e948f
n3dsky <n3dsky@mail.ru>
2025-04-23 22:43:27 +0300

    rabota

--- src/Landing/Footer.jsx
+++ src/Landing/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Footer.css';
 import mast from './images/mast.png'
 import ReactCurvedText from 'react-curved-text';
+import lower_ellipse from './images/Ellipse_down.png'
 
 const Footer = () => {
   const handleLeftClick = () => {
@@ -16,41 +17,46 @@ const Footer = () => {
 
   return (
     <div className="button-container">
-      <button className="image-button left-button" onClick={handleLeftClick}>
-        <img 
-          src={mast} 
-          alt="Left Button" 
-          className="button-image left-button-image"
-        />
-      </button>
+      <div className='footer'>
+        <button className="image-button left-button" onClick={handleLeftClick}>
+          <img 
+            src={mast} 
+            alt="Left Button" 
+            className="button-image left-button-image"
+          />
+        </button>
 
+          
+
+        <div className="center-text">
+          <p>ЖАНР</p>    
+          <ReactCurvedText width='642'
+            height='642'
+            cx='321'
+            cy='321'
+            rx='210'
+            ry='210'
+            startOffset='0'
+            reversed={true}
+            text='ИСТОРИЧЕСКИЙ · БЫТОВОЙ · ПЕЙЗАЖ · НАТЮРМОРТ · ПОРТРЕТ ·'
+            textProps={{"style": {"fontSize": "42", "fontFamily": "Cormorant Infant, serif"}}}
+            textPathProps={null}
+            tspanProps={{"dy": "-60"}}
+            svgProps={{className: "circle-menu", "style": {"transform": "rotate(44deg)"}}} 
+          />
+        </div>
         
-
-      <div className="center-text">
-        <p>ЖАНР</p>    
-        <ReactCurvedText width='642'
-          height='642'
-          cx='321'
-          cy='321'
-          rx='190'
-          ry='190'
-          startOffset='0'
-          reversed={true}
-          text='ИСТОРИЧЕСКИЙ БЫТОВОЙ ПЕЙЗАЖ НАТЮРМОРТ ПОРТРЕТ'
-          textProps={{"style": {"fontSize": "42", "fontFamily": "Cormorant Infant, serif"}}}
-          textPathProps={null}
-          tspanProps={{"dy": "-60"}}
-          svgProps={{className: "circle-menu", "style": {"transform": "rotate(0deg)"}}} 
-        />
+        <button className="image-button right-button" onClick={handleRightClick}>
+          <img 
+            src={mast} 
+            alt="Right Button" 
+            className="button-image right-button-image"
+          />
+        </button>
       </div>
-      
-      <button className="image-button right-button" onClick={handleRightClick}>
-        <img 
-          src={mast} 
-          alt="Right Button" 
-          className="button-image right-button-image"
-        />
-      </button>
+      <div className='lower_ellipse'>
+        <img src={lower_ellipse} />
+      </div>
     </div>
   );
 };
--- src/Landing/Header.jsx
+++ src/Landing/Header.jsx
@@ -1,29 +1,15 @@
 import React from "react";
 import "./Header.css";
+import ellipse from './images/Ellipse_up.png'
 
 export const Header = () => {
     return (
         <div className="header">
-            <div className="frame">
-                <div className="div-wrapper">
-                    <div className="element">текст</div>
-                </div>
-
-                <div className="div-wrapper">
-                    <div className="element">текст</div>
-                </div>
-
-                <div className="div-wrapper">
-                    <div className="text-wrapper">ТЕКСТ</div>
-                </div>
-
-                <div className="div-wrapper">
-                    <div className="element">текст</div>
-                </div>
-
-                <div className="div-wrapper">
-                    <div className="element">текст</div>
-                </div>
+            <div className="upper_ellipse">
+                <img src={ellipse} />
+            </div>
+           <div className="div-wrapper">
+                <div className="text-wrapper">ЗАБЫТЫЕ РУССКИЕ ХУДОЖНИКИ</div>
             </div>
         </div>
     );
